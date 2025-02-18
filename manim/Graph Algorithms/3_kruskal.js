b43adafd3253f91650a2cd2446b5909bfae05558
egeozgul <egeozgul@gmail.com>
2025-02-17 23:55:46 -0500

    Update 3_kruskal.js

--- manim/Graph Algorithms/3_kruskal.js
+++ manim/Graph Algorithms/3_kruskal.js
@@ -82,7 +82,7 @@ const Graph03a = function(s) {
     };
     let tnr;
     s.preload = function() {
-        tnr = s.loadFont('../lib/font/times.ttf');
+        tnr = s.loadFont('https://raw.githubusercontent.com/egeozgul/VizGen/refs/heads/main/manim/lib/font/times.ttf');
     };
     s.setup = function () {
         setup2D(s);
@@ -147,7 +147,7 @@ const Graph03b = function(s) {
     let tnr;
     s.preload = function() {
         //tnr = s.loadFont('../lib/font/times.ttf');
-        tnr = s.loadFont("https://raw.githubusercontent.com/egeozgul/VizGen/refs/heads/main/manim/lib/font/times.ttf");
+        tnr = s.loadFont('https://raw.githubusercontent.com/egeozgul/VizGen/refs/heads/main/manim/lib/font/times.ttf');
     };
     s.setup = function () {
         setup2D(s);
